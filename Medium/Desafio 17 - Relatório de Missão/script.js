759287fa361ef9d782bc2c4f42fe83f4010fe074
Fabiano <fabianoquirinos@outlook.com>
2024-09-19 21:43:20 -0300

    Feat: Questão 17

--- Medium/Desafio 17 - Relatório de Missão/script.js
+++ Medium/Desafio 17 - Relatório de Missão/script.js
@@ -1,1 +1,20 @@
-function tresArgumentos(frase, palavra) {}
+function insertIntoString(str, toInsert, indexes) {
+  const charsArray = str.split("");
+  let insertCount = 0;
+
+  for (let i = 0; i <= str.length; i++) {
+    if (indexes.includes(i)) {
+      charsArray.splice(i + insertCount, 0, toInsert);
+      insertCount++;
+    }
+  }
+  return charsArray.join("");
+}
+insertIntoString("capaz utilizar as cápsulas  emergência", "de", [6, 27]);
+insertIntoString(
+  "Nós decidimos apesar das chances serem baixas que enviaríamos um sinal para [...]",
+  ",",
+  [13, 45]
+);
+insertIntoString("Hello", "world", [6]);
+insertIntoString("Isso é um teste", "não", []);
